1a92752a1a0d7d51c182a39616eba045d548ece6
Darwin Burbano <161047842+EvosZeus@users.noreply.github.com>
2025-02-28 10:29:46 -0500

    Actualizar sensor.ino

--- sensor.ino
+++ sensor.ino
@@ -1,44 +1,40 @@
-// Definición de pines
-const int ledPin = 13;    // LED en pin 13
-const int motorPin = 12;  // Motor en pin 12
-const int sensorPin = A0; // LM35 en A0
+float temp;        
+int sensorValue;   
 
-// Función para leer temperatura
-float readTemperature() {
-    int sensorValue = analogRead(sensorPin);
-    return ((sensorValue * (5.0 / 1023.0))) * 100.0;
-}
+void setup() {    
+  pinMode(13, OUTPUT);    
+  pinMode(12, OUTPUT);    
+  Serial.begin(9600); // Baud rate: Baudios    
+  Serial.println("### Temperature monitor ###");    
+}    
 
-void setup() {
-    pinMode(ledPin, OUTPUT);
-    pinMode(motorPin, OUTPUT);
-    Serial.begin(9600);
-    Serial.println("### Temperature Monitor ###");
-}
+void loop() {    
+  sensorValue = analogRead(A0);    
+  Serial.print("Sensor value: ");    
+  Serial.println(sensorValue);    
+  delay(2000); // miliseconds    
 
-void loop() {
-    float temp = readTemperature();
-    
-    Serial.print("Temp Value: ");
-    Serial.print(temp);
-    Serial.println(" °C");
+  temp = (sensorValue * (500.0 / 1023.0)) - 50.0;    
 
-    // Lógica de activación del LED y motor
-    if (temp < 5) {
-        digitalWrite(ledPin, HIGH);
-        delay(500);
-        digitalWrite(ledPin, LOW);
-        delay(500);
-        digitalWrite(motorPin, LOW);
-    } 
-    else if (temp >= 5 && temp <= 25) {  // Incluye 5°C
-        digitalWrite(ledPin, LOW);
-        digitalWrite(motorPin, LOW);
-    } 
-    else { // temp > 25
-        digitalWrite(ledPin, HIGH);
-        digitalWrite(motorPin, HIGH);
-    }
+  Serial.print("Temp value: ");    
+  Serial.println(temp);    
+  delay(2000);    
 
-    delay(2000);
+  if (temp < 5) {    
+    digitalWrite(13, HIGH);    
+    delay(500);    
+    digitalWrite(13, LOW);    
+    delay(500);    
+    digitalWrite(12, LOW);    
+  }     
+  else if (temp >= 6 && temp <= 25) {    
+    digitalWrite(13, LOW);    
+    digitalWrite(12, LOW);    
+  }     
+  else {    
+    digitalWrite(13, HIGH);    
+    digitalWrite(12, HIGH);    
+  }    
+
+  delay(1000);    
 }
